4b760d6dc3ca54ddba573763362f8b9f98f9bdd2
Vladyslav Sidikov <vlad.sidikov007@gmai.com>
2020-03-17 14:14:49 +0200

    GraphQLID

--- schema/schema.js
+++ schema/schema.js
@@ -1,12 +1,19 @@
 const graphql = require('graphql'); //импорт
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql; //вытягиваем 
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } = graphql; //вытягиваем 
+
+const movies = [
+	{ id: '1', name: 'Pulp Fiction', genre: 'Crime' },
+	{ id: '2', name: '1984', genre: 'Sci-Fi' },
+	{ id: 3, name: 'V for vendetta', genre: 'Sci-Fi-Triller' },
+	{ id: 4, name: 'Snatch', genre: 'Crime-Comedy' },
+];
 
 const MovieType = new GraphQLObjectType({//описываем схему данных хранящихся в базе
   name: 'Movie',
   fields: () => ({//функция возращающая объект данных
-    id: { type: GraphQLString },// GraphQLString обычная строка
-    name: { type: GraphQLString },
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },// GraphQLString обычная строка
     genre: { type: GraphQLString },
   }),
 });
@@ -16,9 +23,9 @@ const Query = new GraphQLObjectType({//корневой запрос
   fields: {
     movie: { //под запрос
       type: MovieType,
-      args: { id: { type: GraphQLString } },
+      args: { id: { type: GraphQLID } },
       resolve(parent, args) {//метод описывающий данные которые должны возвращатся
-
+        return movies.find(movie => movie.id == args.id);
       },
     },
   }
